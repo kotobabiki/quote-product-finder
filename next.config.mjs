2f6df7a1d034feb969c42e7ddce9f5080b2d5429
USER <user@USERnoMacBook-Air.local>
2025-04-28 11:18:14 +0900

    fix: enable appDir for App Router

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true, // ← これを追加するのがポイント！
+    },
+  }
+  
+  export default nextConfig;
+  
