aef3a6559ef1e6583c37de1e0a562d4ebd9d0291
USER <user@USERnoMacBook-Air.local>
2025-04-28 12:20:25 +0900

    fix: set up correct layout and home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-// src/app/layout.tsx
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <html lang="ja">
-        <body>{children}</body>
-      </html>
-    );
-  }
-  
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <p>これはテストページです！</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>これはテストページです！</p>
+    </div>
+  );
+}
